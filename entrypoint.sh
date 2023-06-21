4d0eb8cb6c46caac673c889881b171f05f557c63
lutengda <18346072982@163.com>
2023-06-21 11:53:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,8 @@ urlencode() (
 DEFAULT_POLL_TIMEOUT=10
 POLL_TIMEOUT=${POLL_TIMEOUT:-$DEFAULT_POLL_TIMEOUT}
 
-git checkout "${GITHUB_REF:11}" --
+git checkout "${GITHUB_REF}"
+git checkout "${GITHUB_SHA:11}" --
 
 branch="$(git symbolic-ref --short HEAD)"
 branch_uri="$(urlencode ${branch})"
